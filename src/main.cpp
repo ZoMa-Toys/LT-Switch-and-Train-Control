c1001d5ddb52469910644ffa717dc7e55df62fad
Tamas Guber <dr.gubee@gmail.com>
2021-12-17 12:16:38 +0100

    updated with OTA

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,127 @@
 #include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <AsyncElegantOTA.h>
 #include <WebSocketClient.h>
+#include <WebSerial.h>
 #include <ArduinoJson.h>
-#include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
-
-#define WIFI_SSID "Guber-Kray"
-#define WIFI_PASS "Hafnium1985!"
-
-// Server infos
+const char* ssid = "Guber-Kray";
+const char* password = "Hafnium1985!";
 char* HOST = "guberkray.myftp.org";
 uint16_t PORT = 80;
 uint8_t servo[9] = {0,1,2,3,4,5,6,7};
-uint8_t sensor[9] = {GPIO_NUM_13,GPIO_NUM_12,GPIO_NUM_14,GPIO_NUM_27,GPIO_NUM_15,GPIO_NUM_19,GPIO_NUM_18,GPIO_NUM_4};
+uint8_t sensor[9] = {36,39,34,35,32,33,13,4};
+const int analogThreshold = 3000;
 const long interval = 200;  
-bool activateSensor = true;
+bool activateSensor = false;
 bool debug = false;
+
+
+AsyncWebServer server(80);
+WebSocketClient webSocketClientSwitch;
+WebSocketClient webSocketClientTrain;
+WiFiClient clientSwitch;
+WiFiClient clientTrain;
+
+void SendData(WebSocketClient &wsc, WiFiClient &c ,String &dataToSend){
+  if (c.connected()) {
+    if(dataToSend.length() > 0)
+    {
+      wsc.sendData(dataToSend);
+      if (debug){
+        WebSerial.print("Data Sent: ");
+        WebSerial.println(dataToSend);
+      };
+    }
+    dataToSend = "";
+  } else {
+    WebSerial.println("Client disconnected. (send)");
+    c.connect(HOST, PORT);
+  }
+};
+
+/* Message callback of WebSerial */
+void recvMsg(uint8_t *data, size_t len){
+  WebSerial.println("Received Data...");
+  String d = "";
+  for(int i=0; i < len; i++){
+    d += char(data[i]);
+  }
+  if (d.indexOf(String("SensorOff"))>-1){
+    WebSerial.println("Sensors Deactivated");
+    activateSensor = false;
+  }
+  else if (d.indexOf(String("SensorOn"))>-1){
+    WebSerial.println("Sensors Activated");
+    activateSensor = true;
+  }
+  else if (d.indexOf(String("DebugOff"))>-1){
+    WebSerial.println("Debug Deactivated");
+    debug = false;
+  }
+  else if (d.indexOf(String("DebugOn"))>-1){
+    WebSerial.println("Debug Activated");
+    debug = true;
+  }
+  else if(d.indexOf(String("action"))>-1){
+    SendData(webSocketClientTrain,clientTrain,d);
+  }
+  else if(d.indexOf(String("motor"))>-1){
+    SendData(webSocketClientSwitch,clientSwitch,d);
+  }
+/*   else if(d.indexOf(String("Restart"))>-1){
+    setupFunc();
+  } */
+
+
+  WebSerial.println(d);
+}
+
+
+char initWebSocket(char* wspath,WebSocketClient &wsc, WiFiClient &wifiClient)
+{
+  if(!wifiClient.connect(HOST, PORT)) {
+    WebSerial.println("Connection failed.");
+    return 0;
+  }
+
+  WebSerial.println("Connected.");
+  wsc.path = wspath;
+  wsc.host = HOST;
+  if (!wsc.handshake(wifiClient)) {
+    WebSerial.println("Handshake failed.");
+    return 0;
+  }
+  WebSerial.println("Handshake successful");
+  return 1;
+}
 
 int sensorRead(uint8_t sensorPin){
   if (activateSensor){
-    return digitalRead(sensorPin);
+    if (sensorPin<30){
+      return 1-digitalRead(sensorPin);
+    }
+    else{
+      int sv = analogRead(sensorPin);
+      if (debug){
+        WebSerial.println(sv);
+      }
+      if(sv>analogThreshold){
+        return 0;
+      }
+      else{
+        return 1;
+      }
+    }
   }
   else{
     return 1;
   }
 }
-
-
-WebSocketClient webSocketClientSwitch;
-WebSocketClient webSocketClientTrain;
-WiFiClient clientSwitch;
-WiFiClient clientTrain;
 
 class Switches{
   public:
@@ -83,27 +172,27 @@ class Switches{
     };
     void printValues(){
       if (debug){
-        Serial.print("motor: ");
-        Serial.print(servoPin);
-        Serial.print(" pulse: ");
-        Serial.print(pulse);
-        Serial.print(" printed ");
-        Serial.print(String(printed));
-        Serial.print(" counter ");
-        Serial.print(String(counter));      
-        Serial.print(" lastMillis ");
-        Serial.println(String(lastMillis));
+        WebSerial.print("motor: ");
+        WebSerial.print(servoPin);
+        WebSerial.print(" pulse: ");
+        WebSerial.print(pulse);
+        WebSerial.print(" printed ");
+        WebSerial.print(String(printed));
+        WebSerial.print(" counter ");
+        WebSerial.print(String(counter));      
+        WebSerial.print(" lastMillis ");
+        WebSerial.println(String(lastMillis));
       }
     }
   private:
     void turnServo(){
       if (debug){
-        Serial.print("Counter ");
-        Serial.println(String(counter));
-        Serial.print("Turn servo ");
-        Serial.print(String(pulse));
-        Serial.print(" on ");
-        Serial.println(String(servoPin));
+        WebSerial.print("Counter ");
+        WebSerial.println(String(counter));
+        WebSerial.print("Turn servo ");
+        WebSerial.print(String(pulse));
+        WebSerial.print(" on ");
+        WebSerial.println(String(servoPin));
       }
       pwm.setPWM(servoPin, 0, pulse); 
       if (printed){
@@ -117,18 +206,17 @@ class Switches{
 int NumOFSwitches=0;
 Switches switches[15];
 
-
-
 String dataToSend; // update this with the value you wish to send to the server
 
 
 void onDataReceived(String &data)
 {
+
   DynamicJsonDocument jsoninput(2048);
   DeserializationError error = deserializeJson(jsoninput, data);
   if (error) {
-    Serial.print(F("deserializeJson() failed: "));
-    Serial.println(error.f_str());
+    WebSerial.print(F("deserializeJson() failed: "));
+    WebSerial.println(error.f_str());
     return;
   }
   if (jsoninput.containsKey("motor")){   
@@ -150,157 +238,85 @@ void onDataReceived(String &data)
     }
   }
   else if (jsoninput.containsKey("SensorConfig")){
-    Serial.println("SensorConfig");
     JsonArray SensorConfig = jsoninput["SensorConfig"].as<JsonArray>();
-    Serial.println(SensorConfig);
+    if (debug){
+      WebSerial.println(SensorConfig);
+    }
     int i = 0;
     for (JsonVariant senconf : SensorConfig) {
       sensor[i]=senconf.as<uint8_t>();
       i++;
       if(debug){
-        Serial.print("Sensor ");
-        Serial.print(String(i));
-        Serial.print(" Pin ");
-        Serial.println(String(sensor[i]));
+        WebSerial.print("Sensor ");
+        WebSerial.print(String(i));
+        WebSerial.print(" Pin ");
+        WebSerial.println(String(sensor[i]));
       }
     }
   }
 }
 
-char initWebSocket(char* wspath,WebSocketClient &wsc,WiFiClient &client)
-{
-  if(!client.connect(HOST, PORT)) {
-    Serial.println("Connection failed.");
-    return 0;
-  }
 
-  Serial.println("Connected.");
-  wsc.path = wspath;
-  wsc.host = HOST;
-  if (!wsc.handshake(client)) {
-    Serial.println("Handshake failed.");
-    return 0;
-  }
-  Serial.println("Handshake successful");
-  return 1;
-}
-
-
-char connect()
-{
-  WiFi.begin(WIFI_SSID, WIFI_PASS); 
-
-  Serial.println("Waiting for wifi");
-  int timeout_s = 30;
-  while (WiFi.status() != WL_CONNECTED && timeout_s-- > 0) {
-      delay(1000);
-      Serial.print(".");
-  }
-  
-  if(WiFi.status() != WL_CONNECTED)
-  {
-    Serial.println("unable to connect, check your credentials");
-    return 0;
-  }
-  else
-  {
-    Serial.println("Connected to the WiFi network");
-    Serial.println(WiFi.localIP());
-    return 1;
-  }
-}
-
-void SendData(WebSocketClient &wsc, WiFiClient &c ,String &dataToSend){
-  if (c.connected()) {
-    if(dataToSend.length() > 0)
-    {
-      wsc.sendData(dataToSend);
-      if (debug){
-        Serial.print("Data Sent: ");
-        Serial.println(dataToSend);
-      };
-    }
-    dataToSend = "";
-  } else {
-    Serial.println("Client disconnected. (send)");
-    c.connect(HOST, PORT);
-  }
-};
-void GetData(WebSocketClient &wsc, WiFiClient &c){
+void GetData(WebSocketClient &wsc,WiFiClient &wifiClient){
   String data;
  
-  if (c.connected()) {
+  if (wifiClient.connected()) {
     wsc.getData(data);
     if (data.length() > 0) {
       onDataReceived(data);
     }
   } else {
-    Serial.println("Client disconnected. (get)");
-    c.connect(HOST, PORT);
+    WebSerial.println("Client disconnected. (get)");
+    wifiClient.connect(HOST, PORT);
   }
 };
 
-void setupFunc()
-{
+void setupFunc(){
   Serial.begin(115200);
-  // try to connect to the wifi
-  Serial.println("Wifi:");
-  if(connect() == 0) { return ; };
-  // once connected to the wifi, let's reach our server
-  if(initWebSocket("/ws/switch",webSocketClientSwitch,clientSwitch) == 0) { return ; };
-  if(initWebSocket("/ws/train",webSocketClientTrain,clientTrain) == 0) { return ; };
-  dataToSend = "{\"action\":\"getConfigESP\"}";
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+/*   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(200, "text/plain", "Hi! I am ESP32.");
+  }); */
+  WebSerial.begin(&server);
+    /* Attach Message Callback */
+  WebSerial.msgCallback(recvMsg);
+  AsyncElegantOTA.begin(&server);    // Start ElegantOTA
+  server.begin();
+  WebSerial.println("HTTP server started");
+  if(initWebSocket("/switch",webSocketClientSwitch, clientSwitch) == 0) { return ; };
+  if(initWebSocket("/train",webSocketClientTrain, clientTrain) == 0) { return ; };  
   pwm.begin();
   pwm.setOscillatorFrequency(27000000);
   pwm.setPWMFreq(50); 
+  dataToSend = "{\"action\":\"getConfigESP\"}";
+  SendData(webSocketClientTrain,clientTrain,dataToSend);
 }
 
-void setup()
-{
+void setup(void) {
   setupFunc();
 }
 
 
-
-void loop() {
-  if (Serial.available()) {
-    dataToSend=Serial.readString();
-  }
-  if (dataToSend.indexOf(String("SensorOff"))>-1){
-    Serial.println("Sensors Deactivated");
-    activateSensor = false;
-    dataToSend = "";
-  }
-  else if (dataToSend.indexOf(String("SensorOn"))>-1){
-    Serial.println("Sensors Activated");
-    activateSensor = true;
-    dataToSend = "";
-  }
-  else if (dataToSend.indexOf(String("DebugOff"))>-1){
-    Serial.println("Debug Deactivated");
-    debug = false;
-    dataToSend = "";
-  }
-  else if (dataToSend.indexOf(String("DebugOn"))>-1){
-    Serial.println("Debug Activated");
-    debug = true;
-    dataToSend = "";
-  }
-  else if(dataToSend.indexOf(String("action"))>-1){
-    SendData(webSocketClientTrain,clientTrain,dataToSend);
-  }
-  else if(dataToSend.indexOf(String("motor"))>-1){
-    SendData(webSocketClientSwitch,clientSwitch,dataToSend);
-  }
-  else if(dataToSend.indexOf(String("Restart"))>-1){
-    setupFunc();
-  }
-
+void loop(void) {
   GetData(webSocketClientSwitch,clientSwitch);
   GetData(webSocketClientTrain,clientTrain);
-  
   for (int i = 0; i < NumOFSwitches ; i++){
     dataToSend = switches[i].getSensorStatus();
+    switches[i].printValues();
     if(dataToSend.length() > 0){
       SendData(webSocketClientSwitch,clientSwitch,dataToSend);
     }
